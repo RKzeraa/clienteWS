ba373d185b5ad534d6f24ec8718b2611a42f6556
Ramom Karllos <rkzeraa@gmail.com>
2022-08-15 13:44:11 -0300

    [Feat] - Client WebService Gerador de Senha

---
+++ GeradorDeSenha/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace GeradorDeSenha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb1.AutoSize = true;
+            this.lb1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lb1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(87, 70);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(146, 37);
+            this.lb1.TabIndex = 2;
+            this.lb1.Text = "Nova Senha";
+            this.lb1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lb1.UseCompatibleTextRendering = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 20.29091F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(83, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 77);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GERAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(337, 246);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Gerenciador de Senha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GeradorDeSenha/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorDeSenha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceReference1.servicoSoapClient ws = new ServiceReference1.servicoSoapClient();
+                this.lb1.Text = ws.CriarSenha();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro 500");
+            }
+        
+        }
+    }
+}
